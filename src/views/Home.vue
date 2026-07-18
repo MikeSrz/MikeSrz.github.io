b880902cbeb492acb4ad448cfb4f4b8eb77afb09
Michael Suárez <onriosrz@gmail.com>
2026-07-18 16:56:17 +0200

    Ajuste en texto

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,12 @@
   <section class="mx-auto max-w-5xl px-4 py-8 sm:px-6 lg:px-8">
     <div class="space-y-5 text-center">
       <h1 class="text-4xl text-white">Michael Suárez Botero</h1>
-      <h3>Junior Backend Developer</h3>
+      <h3>Junior FullStack Developer</h3>
       <img src="@/assets/profile.png" alt="Profile Picture" class="profile-image" />
       <p class="mt-2 text-xl text-slate-500 text-left dark:text-slate-400">
-        Soy desarrollador backend con experiencia en aplicaciones profesionales como desarrollador fullstack<br>
-        He implementado funcionalidades de backend para herramientas financieras, optimización de procesos y corrección de bugs, siempre buscando una mejor experiencia de usuario y mayor eficiencia.<br>
-        Entre mis logros está la reducción de hasta un 90 % en tiempos de consulta y la dockerización completa de proyectos para despliegues más rápidos y seguros en entornos de desarrollo.
+        Soy desarrollador backend con experiencia de prácticas en entornos profesionales como desarrollador fullstack<br>
+        He implementado funcionalidades de backend para herramientas financieras, optimización de procesos y corrección de bugs, siempre procurando enfocar mis desarrollos a la mayor eficiencia y escalabilidad.<br>
+         Entre mis logros está la reducción de hasta un 90 % en tiempos de consulta optimizando bases de datos y refactorizando código en Laravel.
       </p>
     </div>
     <HomeNav/>
